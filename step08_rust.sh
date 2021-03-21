5d072ac89e2c3c29defff993be395895d9bdad32
Tim HC Lin (林弘佶) <timhc.lin@moxa.com>
2021-03-20 22:01:01 -0700

    Refactor

--- step08_rust.sh
+++ step08_rust.sh
@@ -4,3 +4,6 @@ sudo apt update
 sudo apt upgrade -y
 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+cargo install cross
+cargo install cargo-license
--- step09.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cargo install cross
-cargo install cargo-license
-
