b0b57de8f76460f76d0948c8661f253bb6a6749a
Tim HC Lin (林弘佶) <timhc.lin@moxa.com>
2021-03-23 01:33:31 -0700

    Steps for Rust bug fix

--- step08_rust.sh
+++ step08_rust.sh
@@ -5,5 +5,3 @@ sudo apt upgrade -y
 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
-cargo install cross
-cargo install cargo-license
---
+++ step09_rust.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cargo install cross
+cargo install cargo-license
