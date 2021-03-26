90039a5dd97003829c22daaf5cf404c3f79fc8f9
Tim HC Lin (林弘佶) <timhc.lin@moxa.com>
2021-03-26 02:10:17 -0700

    Add subl

--- step05_docker.sh
+++ step05_docker.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-
-sudo systemctl status docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo docker -v
