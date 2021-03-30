d9c22b3749004ece3e42fba04571d457aebacc7b
Tim HC Lin (林弘佶) <timhc.lin@moxa.com>
2021-03-30 14:39:06 +0800

    Add zsh

---
+++ step21_zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install zsh -y
+sudo apt install powerline fonts-powerline -y
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Modify ~/.zshrc ==> plugins=(zsh-autosuggestions)"
